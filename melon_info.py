48a465e8dee4b101ae74e43b0266be8948fc511f
Ruth Rosenthal <ruthbrosenthal@gmail.com>
2021-07-29 04:34:13 +0000

    placed all melon data in single dictionary

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,19 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melon_names
 
 
-def print_melon(name, seedless, price):
-    """Print each melon with corresponding attribute information."""
-
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
-
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+def print_melon(melon_dict):
+    """Takes in melon dictionary and prints each melon with corresponding attribute information."""
+    for melon in melon_dict:
+        print(melon.upper())
+        print(f"\ttab test")
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+print_melon(melon_names)
+#     print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+
+
+# for i in melon_names:
+#     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
